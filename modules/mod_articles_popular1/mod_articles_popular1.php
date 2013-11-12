21a010e68f47d6806e9ce091d9558388c113ee61
Galene <galina.ck@gmail.com>
2013-11-12 15:40:56 +0200

    remove

--- modules/mod_articles_popular1/mod_articles_popular1.php
+++
@@ -1,6 +0,0 @@
-<?php
-        defined('_JEXEC') or die('Restricted access');
-        require_once( dirname(__FILE__).DS.'helper.php' );
-        $data = modTestHelper::getData($params);
-        require(JModuleHelper::getLayoutPath('mod_articles_popular1'));
-?>
--- modules/mod_articles_popular1/tmpl/default.php
+++
@@ -1,8 +0,0 @@
-<?php
-        defined('_JEXEC') or die('Restricted access');
-?>
-<ul>
-        <?php foreach($data as $item): ?>
-        <li><a href="index.php?option=com_content&view=article&id=<?php echo $item->id ?>"><?php print_r($item->title); ?></a></li>
-        <?php endforeach; ?>
-</ul>
